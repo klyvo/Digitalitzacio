45345fe2d14ab5b6465cce382c42a87a43fe8e28
harkaproven-svg <harkaproven@gmail.com>
2025-09-29 14:51:03 +0200

    prueba

--- A1/suma.py
+++ A1/suma.py
@@ -1,1 +1,12 @@
-a=input("")
+a = input("Di un numero: ")
+if(a.isnumeric()):
+    print("n es numeric")
+    reste = int(a)%2
+    if reste == 0:
+        print("Aquest numero es parell.")
+    else:
+        print("Aquest numero es parell")
+
+
+else:
+    print("eres tonto?")
