c994afb874bf36c5cc05983bfa040140c3df82d9
harkaproven-svg <harkaproven@gmail.com>
2025-10-01 13:56:30 +0200

    aggg

---
+++ A1/celsios.py
@@ -0,0 +1,4 @@
+celsius = input("Di los grados celsios: ")
+far = ((int(celsius) * (9/5) ) +32)
+
+print(celsius ," son ", far ,"grados farenheit")
---
+++ requisits.md
@@ -0,0 +1,6 @@
+# Entorns de desenvolupament 26-26
+
+Entorns de desenvolupament 25-26
+
+## Requisits Funcionals Tapatapp
+
